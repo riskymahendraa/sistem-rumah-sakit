1573e977abb906d7e355ae5abdcfd426da6ccb15
Risky Mahendra <riskymahendra52@gmail.com>
2025-06-20 21:09:17 +0800

    doctor-section

---
+++ resources/js/Pages/Admin/Dokter/Create.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import DashboardLayout from "@/Layouts/DashboardLayout";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import { Head } from "@inertiajs/react";
+
+export default function create() {
+    return (
+        <DashboardLayout>
+            <Head title="Tambah Data Dokter" />
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6} md={4}>
+                    <Box p={2}>
+                        <div className="font-bold text-2xl">
+                            Tambah Data Dokter
+                        </div>
+                    </Box>
+                </Grid>
+            </Grid>
+        </DashboardLayout>
+    );
+}
