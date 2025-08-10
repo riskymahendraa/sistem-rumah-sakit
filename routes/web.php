ba6e58cd5f03c654daa7282495bfe4b435a3a8fe
Risky Mahendra <riskymahendra52@gmail.com>
2025-08-10 20:58:39 +0800

    refactor: consolidate route definitions under auth middleware for better organization

--- routes/web.php
+++ routes/web.php
@@ -9,45 +9,17 @@ use App\Http\Controllers\PatientController;
 use App\Http\Controllers\DashboardController;
 use Inertia\Inertia;
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-
-
-// route CRUD doctor
-// route::resource('doctor', DoctorController::class);
-
-// routes/web.php
-Route::get('doctor', [DoctorController::class, 'index'])->name('doctor.index');
-Route::get('doctor/create', [DoctorController::class, 'create'])->name('doctor.create');
-Route::post('doctor', [DoctorController::class, 'store'])->name('doctor.store');
-Route::get('doctor/{doctor}/edit', [DoctorController::class, 'edit'])->name('doctor.edit');
-Route::get('doctor/{doctor}', [DoctorController::class, 'show'])->name('doctor.show');
-Route::put('doctor/{doctor}', [DoctorController::class, 'update'])->name('doctor.update');
-Route::delete('doctor/{doctor}', [DoctorController::class, 'destroy'])->name('doctor.destroy');
-
-
-Route::get('room', [RoomController::class, 'index'])->name('room.index');
-Route::get('room/create', [RoomController::class, 'create'])->name('room.create');
-Route::post('room', [RoomController::class, 'store'])->name('room.store');
-Route::get('room/{room}/edit', [RoomController::class, 'edit'])->name('room.edit');
-Route::put('room/{room}', [RoomController::class, 'update'])->name('room.update');
-Route::get('room/{room}', [RoomController::class, 'show'])->name('room.show');
-Route::delete('room/{room}', [RoomController::class, 'destroy'])->name('room.destroy');
-
-
-Route::get('patient', [PatientController::class, 'index'])->name('patient.index');
-Route::get('patient/create', [PatientController::class, 'create'])->name('patient.create');
-Route::post('patient', [PatientController::class, 'store'])->name('patient.store');
-Route::get('patient/{patient}/edit', [PatientController::class, 'edit'])->name('patient.edit');
-Route::put('patient/{patient}', [PatientController::class, 'update'])->name('patient.update');
-Route::get('patient/{patient}', [PatientController::class, 'show'])->name('patient.show');
-Route::delete('patient/{patient}', [PatientController::class, 'destroy'])->name('patient.destroy');
-
-
-
 Route::middleware('auth')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::resource('doctor', DoctorController::class);
+    Route::resource('room', RoomController::class);
+    Route::resource('patient', PatientController::class);
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__.'/auth.php';
